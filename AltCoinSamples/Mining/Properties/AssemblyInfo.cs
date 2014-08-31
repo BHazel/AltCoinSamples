11a16a435c0482db37b28f18c584aad7d88ac40f
Benedict Hazel <bwhazel@gmail.com>
2014-08-31 01:15:18 +0100

    Update Mining assembly information.

--- AltCoinSamples/Mining/Properties/AssemblyInfo.cs
+++ AltCoinSamples/Mining/Properties/AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mining")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simulates the mining process.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Mining")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Benedict W. Hazel 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -54,5 +54,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
