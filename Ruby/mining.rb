18ce03230f91ac14fde38e9cbb8326e5057d0040
Benedict Hazel <bwhazel@gmail.com>
2014-09-01 23:42:23 +0100

    Implement generic block solving method and target setters.

--- Ruby/mining.rb
+++ Ruby/mining.rb
@@ -3,6 +3,34 @@ require "Digest"
 
 $hasher = Digest::MD5.new
 
+def get_hash(input)
+  return $hasher.reset.update(input).to_s
+end
+
+def solve_block(input, target, mode)
+  nonce = 0
+  solved = false
+  loop do
+    input_with_nonce = input + nonce.to_s
+    hash = get_hash(input_with_nonce)
+    nonce = nonce + 1
+
+    if (mode == "zero") then
+      if (hash.start_with?(target)) then
+        solved = true
+      end
+    else
+      if (hash.hex < target) then
+        solved = true
+      end
+    end
+
+    if (solved) then
+      return [hash, nonce]
+    end
+  end
+end
+=begin
 def solve_block_zero(input, target)
   hash = ""
   nonce = 0
@@ -27,7 +55,7 @@ def solve_block_limit(input, target)
     end
   end
 end
-
+=end
 #./mining.rb input -b 100 -m zero|limit|text|auto -t target -a
 
 opts = Slop.parse! do
@@ -46,9 +74,17 @@ if (mode != "zero" && mode != "limit" && mode != "text" && mode != "auto") then
   exit(1)
 end
 
+if (mode == "limit") then
+  target = target.hex
+elsif (mode == "word") then
+  target = get_hash(target)
+else
+  target = Time.now.to_s
+end
+
 solved_block = [input, 0]
 (1..blocks).each do |b|
-  solved_block = solve_block_zero(solved_block[0], target)
+  solved_block = solve_block(solved_block[0], target, mode)
   puts "Block #{b}:"
   puts "  Hash  -> #{solved_block[0]}"
   puts "  Count -> #{solved_block[1]}"
