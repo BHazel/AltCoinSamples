ab24f27549719c2577cee0baba1f4b04bb24fe82
Benedict Hazel <bwhazel@gmail.com>
2014-09-06 23:55:06 +0100

    Add view-model for the Mining window.

---
+++ AltCoinSamples/Mining/ViewModels/MiningViewModel.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="MiningViewModel.cs" company="Benedict W. Hazel">
+//     Benedict W. Hazel, 2014
+// </copyright>
+// <author>Benedict W. Hazel</author>
+// <summary>
+//     MiningViewModel: Class for the Mining window view-model.
+// </summary>
+
+namespace BWHazel.Apps.AltCoinSamples.Mining.ViewModels
+{
+    using System.ComponentModel;
+    using BWHazel.Apps.AltCoinSamples.Mining.Models;
+
+    /// <summary>
+    /// Mining window view-model.
+    /// </summary>
+    public class MiningViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
